d65a9f603beab8a5c6db97f9a4f1986124b449bf
Daniele Esposti <daniele@withplum.com>
2024-11-09 01:34:53 +0000

    feat: Dropped support for Python 3.8

--- incident_io_client/models/alert_result.py
+++ incident_io_client/models/alert_result.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Type, TypeVar
+from typing import Any, TypeVar
 
 from attrs import define as _attrs_define
 from attrs import field as _attrs_field
@@ -27,16 +27,16 @@ class AlertResult:
     deduplication_key: AlertResultDeduplicationKey
     message: AlertResultMessage
     status: AlertResultStatus
-    additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
+    additional_properties: dict[str, Any] = _attrs_field(init=False, factory=dict)
 
-    def to_dict(self) -> Dict[str, Any]:
+    def to_dict(self) -> dict[str, Any]:
         deduplication_key = self.deduplication_key.value
 
         message = self.message.value
 
         status = self.status.value
 
-        field_dict: Dict[str, Any] = {}
+        field_dict: dict[str, Any] = {}
         field_dict.update(self.additional_properties)
         field_dict.update(
             {
@@ -49,7 +49,7 @@ class AlertResult:
         return field_dict
 
     @classmethod
-    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+    def from_dict(cls: type[T], src_dict: dict[str, Any]) -> T:
         d = src_dict.copy()
         deduplication_key = AlertResultDeduplicationKey(d.pop("deduplication_key"))
 
@@ -67,7 +67,7 @@ class AlertResult:
         return alert_result
 
     @property
-    def additional_keys(self) -> List[str]:
+    def additional_keys(self) -> list[str]:
         return list(self.additional_properties.keys())
 
     def __getitem__(self, key: str) -> Any:
